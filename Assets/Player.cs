67d4ade09d8a561c0814fc6af6b9b54e1ef4cbdb
Peter Glenn <peter.glenn.17@cnu.edu>
2020-01-08 21:41:49 -0500

    Fixed bug where 2D box collider was uninitialized on game start for the player.

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -5,12 +5,12 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     private BoxCollider2D boxCollider;
-
     private Vector3 moveDelta;
+    private RaycastHit2D hit;
 
     private void Start()
     {
-        boxCollider.GetComponent<BoxCollider2D>();
+        boxCollider = GetComponent<BoxCollider2D>();
     }
 
     private void FixedUpdate()
@@ -30,9 +30,24 @@ public class Player : MonoBehaviour
         {
             transform.localScale = new Vector3(-1, 1, 1);
         }
+        
+        // Check if player can move in the y direction by casting a box there first. If the box returns null, player can move
+        hit = Physics2D.BoxCast(transform.position, boxCollider.size, 0, new Vector2(0, moveDelta.y), Mathf.Abs(moveDelta.y * Time.deltaTime), LayerMask.GetMask("Actor", "Blocking"));
+        if(hit.collider == null)
+        {
+            // Player Movement
+            transform.Translate(0, moveDelta.y * Time.deltaTime, 0);
+        }
 
-        // Player Movement
-        transform.Translate(moveDelta * Time.deltaTime);
+        // Check if player can move in the x direction by casting a box there first. If the box returns null, player can move
+        hit = Physics2D.BoxCast(transform.position, boxCollider.size, 0, new Vector2(moveDelta.x, 0), Mathf.Abs(moveDelta.x * Time.deltaTime), LayerMask.GetMask("Actor", "Blocking"));
+        if (hit.collider == null)
+        {
+            // Player Movement
+            transform.Translate(moveDelta.x * Time.deltaTime, 0, 0);
+        }
+        
+
     }
 
 }
